39bad820cbb5633a04239f5f464a47570e0c1b98
itorresramg <ivanpranimacion@gmail.com>
2025-05-17 21:30:00 +0200

    FINAL

--- config/db.js
+++ config/db.js
@@ -6,7 +6,8 @@ const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 };
 
 const connection = mysql.createConnection(dbConfig);
