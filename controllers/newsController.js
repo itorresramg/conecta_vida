2a99fb0e8aeaf6a29da6ba230c02bf231c3b677b
itorresramg <ivanpranimacion@gmail.com>
2025-05-17 22:23:35 +0200

    Errores

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -20,16 +20,24 @@ exports.getNews = async (req, res) => {
         to: hoy,
         sortBy: 'publishedAt', // Ordenadas por fecha de publicación
         language: 'es',
-        pageSize: 10        
+        pageSize: 10
       },
-      headers:{
-        'X-Api-Key':apiKey
+      headers: {
+        'X-Api-Key': apiKey
       }
     });
 
     res.status(200).json(resultado.data.articles);
   } catch (err) {
-    console.error('Hubo un problema al recuperar las noticias:', err.response?.data || err.message,err.code);
-    res.status(500).json({ message: 'No se pudieron cargar las noticias en este momento' });
+    console.error('❌ Error completo en /api/news:');
+    if (err.response) {
+      console.error('🧾 Código:', err.response.status);
+      console.error('📩 Data:', err.response.data);
+    } else if (err.request) {
+      console.error('🌐 Error de red o sin respuesta:', err.request);
+    } else {
+      console.error('❗ Error general:', err.message);
+    }
+    res.status(500).json({ message: 'No se pudieron cargar las noticias en este momento' })
   }
 };
